9bbaaa7884dcdbc5cca761f8b06d8509b0f0135c
marcus <marcusmissing@gmail.com>
2020-07-01 18:47:03 +0200

    Server classify

--- src/KI/config.py
+++ src/KI/config.py
@@ -1,5 +1,5 @@
 # ---------------- config params ----------------
-image_size_gesture = (256, 256, 3)
+image_size_gesture = (200, 200, 3)
 image_size_pong = (100, 100, 3)
 batch_size = 32
 epochs = 150
