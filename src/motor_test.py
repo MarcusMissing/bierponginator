d4af55bad9dd22631b118a2417c7b3befe4a9f5f
marcus <marcusmissing@gmail.com>
2020-08-18 14:47:00 +0200

    motor_test.py

--- src/motor_test.py
+++ src/motor_test.py
@@ -1,34 +1,58 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-DIR = 20   # Direction GPIO Pin
-STEP = 21  # Step GPIO Pin
-CW = 0     # Clockwise Rotation
-CCW = 1    # Counterclockwise Rotation
-SPR = 200   # Steps per Revolution (360 / 7.5)
+DIR_1 = 20
+STEP_1 = 21
+DIR_2 = 16
+STEP_2 = 12
+CW = 0
+CCW = 1
+SPR = 200
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
+GPIO.setup(DIR_1, GPIO.OUT)
+GPIO.setup(STEP_1, GPIO.OUT)
+GPIO.output(DIR_1, CW)
 
 step_count = SPR
 delay_CW = .00001
 delay_CCW = .0005
 
 for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
+    GPIO.output(STEP_1, GPIO.HIGH)
     sleep(delay_CW)
-    GPIO.output(STEP, GPIO.LOW)
+    GPIO.output(STEP_1, GPIO.LOW)
     sleep(delay_CW)
     print(x)
 
 sleep(.5)
-GPIO.output(DIR, CCW)
+GPIO.output(DIR_1, CCW)
 for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
+    GPIO.output(STEP_1, GPIO.HIGH)
     sleep(delay_CCW)
-    GPIO.output(STEP, GPIO.LOW)
+    GPIO.output(STEP_1, GPIO.LOW)
+    sleep(delay_CCW)
+
+sleep(1)
+
+# GPIO.cleanup()
+
+GPIO.setup(DIR_2, GPIO.OUT)
+GPIO.setup(STEP_2, GPIO.OUT)
+
+for x in range(step_count):
+    GPIO.output(STEP_2, GPIO.HIGH)
+    sleep(delay_CW)
+    GPIO.output(STEP_2, GPIO.LOW)
+    sleep(delay_CW)
+    print(x)
+
+sleep(.5)
+GPIO.output(DIR_2, CCW)
+for x in range(step_count):
+    GPIO.output(STEP_2, GPIO.HIGH)
+    sleep(delay_CCW)
+    GPIO.output(STEP_2, GPIO.LOW)
     sleep(delay_CCW)
 
 GPIO.cleanup()
