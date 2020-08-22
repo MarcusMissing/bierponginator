32815308bf273dd42e25f60d5708384d6a34d42b
marcus <marcusmissing@gmail.com>
2020-08-22 10:33:19 +0200

    motor

--- src/motor_test.py
+++ src/motor_test.py
@@ -1,18 +1,14 @@
 from time import sleep
+
 import RPi.GPIO as GPIO
 
-DIR_1 = 20
-STEP_1 = 21
-DIR_2 = 26
-STEP_2 = 19
-CW = 0
-CCW = 1
+import KI.config as config
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR_1, GPIO.OUT)
-GPIO.setup(STEP_1, GPIO.OUT)
-GPIO.setup(DIR_2, GPIO.OUT)
-GPIO.setup(STEP_2, GPIO.OUT)
+GPIO.setup(config.DIR_1, GPIO.OUT)
+GPIO.setup(config.STEP_1, GPIO.OUT)
+GPIO.setup(config.DIR_2, GPIO.OUT)
+GPIO.setup(config.STEP_2, GPIO.OUT)
 
 
 def motor_test(DIR_pin, DIR, STEP_pin, steps, delay):
@@ -24,10 +20,10 @@ def motor_test(DIR_pin, DIR, STEP_pin, steps, delay):
         sleep(delay)
 
 
-motor_test(DIR_1, CW, STEP_1, steps=50, delay=.005)
-motor_test(DIR_1, CCW, STEP_1, steps=50, delay=.005)
+motor_test(config.DIR_1, config.CW, config.STEP_1, steps=50, delay=.005)
+motor_test(config.DIR_1, config.CCW, config.STEP_1, steps=50, delay=.005)
 
-motor_test(DIR_2, CW, STEP_2, steps=50, delay=.001)
-motor_test(DIR_2, CCW, STEP_2, steps=50, delay=.002)
+motor_test(config.DIR_2, config.CW, config.STEP_2, steps=50, delay=.001)
+motor_test(config.DIR_2, config.CCW, config.STEP_2, steps=50, delay=.002)
 
 GPIO.cleanup()
