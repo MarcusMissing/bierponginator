68d9f388b6f2ba98048e73e51d7d0afef4cda9fa
SimonGoldhofer <simon.goldhofer@gmail.com>
2020-12-17 13:54:02 +0100

    stuff changed

--- src/motor_test.py
+++ src/motor_test.py
@@ -9,6 +9,7 @@ MOTOR_Z_DIR_PIN = 20
 Motor_Z = 21
 MOTOR_X_DIR_PIN = 26
 Motor_X = 19
+
 CW = 0
 CCW = 1
 
@@ -33,9 +34,12 @@ def motor_test(DIR_pin, DIR, STEP_pin, steps, delay):
 def ramp_motor(direction_pin, direction, motor_pin, nm_steps, sps):
     print("Set Steps per Second to: {}".format(sps))
     GPIO.output(direction_pin, direction)
+    initial_delay = 0.05
+    delay_decrease = (initial_delay - (1 / sps)) / nm_steps
     for x in range(1, nm_steps):
         GPIO.output(motor_pin, GPIO.HIGH)
-        delay = 1/(sps * (1 / (nm_steps - x)))
+        delay = round(initial_delay - delay_decrease * x, 5)
+        # delay = 1/(sps * (1 / (nm_steps - x)))
         sleep(delay)
         GPIO.output(motor_pin, GPIO.LOW)
         sleep(delay)
@@ -49,6 +53,6 @@ init_GPIO()
 # motor_test(config.DIR_2, config.CW, config.STEP_2, steps=50, delay=.001)
 # motor_test(config.DIR_2, config.CCW, config.STEP_2, steps=50, delay=.002)
 
-ramp_motor(MOTOR_X_DIR_PIN, CW, Motor_X, 60, 1000)
+ramp_motor(MOTOR_X_DIR_PIN, CW, Motor_X, 60, 10000)
 
 GPIO.cleanup()
