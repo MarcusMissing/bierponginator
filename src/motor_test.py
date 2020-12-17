e38af8a48edfce33d5b53990bd3c003a22d4b382
SimonGoldhofer <simon.goldhofer@gmail.com>
2020-12-17 13:14:24 +0100

    stuff changed

--- src/motor_test.py
+++ src/motor_test.py
@@ -1,16 +1,26 @@
 from time import sleep
 
 import RPi.GPIO as GPIO
+from numpy.testing._private.parameterized import param
 
 import KI.config as config
+
+CW_PIN = 20
+Motor_Z = 21
+CCW_PIN = 26
+Motor_X = 19
+CW = 0
+CCW = 1
+
+pi = True
 
 
 def init_GPIO():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(config.DIR_1, GPIO.OUT)
-    GPIO.setup(config.STEP_1, GPIO.OUT)
-    GPIO.setup(config.DIR_2, GPIO.OUT)
-    GPIO.setup(config.STEP_2, GPIO.OUT)
+    GPIO.setup(CW_PIN, GPIO.OUT)
+    GPIO.setup(Motor_Z, GPIO.OUT)
+    GPIO.setup(CCW_PIN, GPIO.OUT)
+    GPIO.setup(Motor_Z, GPIO.OUT)
 
 
 def motor_test(DIR_pin, DIR, STEP_pin, steps, delay):
@@ -22,12 +32,25 @@ def motor_test(DIR_pin, DIR, STEP_pin, steps, delay):
         sleep(delay)
 
 
+def ramp_motor(direction_pin, direction, motor_pin, steps, sps):
+    print("Set Steps per Second to: {}".format(sps))
+    GPIO.output(direction_pin, direction)
+    for x in range(1, steps):
+        GPIO.output(motor_pin, GPIO.HIGH)
+        delay = 1/(sps*(1/(steps-x)))
+        sleep(delay)
+        GPIO.output(motor_pin, GPIO.LOW)
+        sleep(delay)
+
+
 init_GPIO()
 
-motor_test(config.DIR_1, config.CW, config.STEP_1, steps=50, delay=.005)
-motor_test(config.DIR_1, config.CCW, config.STEP_1, steps=50, delay=.005)
+# motor_test(config.DIR_1, config.CW, config.STEP_1, steps=50, delay=.005)
+# motor_test(config.DIR_1, config.CCW, config.STEP_1, steps=50, delay=.005)
 
-motor_test(config.DIR_2, config.CW, config.STEP_2, steps=50, delay=.001)
-motor_test(config.DIR_2, config.CCW, config.STEP_2, steps=50, delay=.002)
+# motor_test(config.DIR_2, config.CW, config.STEP_2, steps=50, delay=.001)
+# motor_test(config.DIR_2, config.CCW, config.STEP_2, steps=50, delay=.002)
+
+ramp_motor(CCW_PIN, CCW, Motor_X, 60, 1000)
 
 GPIO.cleanup()
