5bd428b481535c2cd43ef0dc430e28c8e52ce88b
SimonGoldhofer <simon.goldhofer@gmail.com>
2020-12-18 19:51:35 +0100

    stuff changed

--- src/motor_test.py
+++ src/motor_test.py
@@ -1,8 +1,7 @@
 from time import sleep
-
+import itertools
 import RPi.GPIO as GPIO
-from numpy.testing._private.parameterized import param
-
+import numpy as np
 import KI.config as config
 
 MOTOR_Z = {"dir_pins": [20],
@@ -15,46 +14,47 @@ CW = 0
 CCW = 1
 
 
-def init_GPIO():
+def test_motor(motor,
+               direction,
+               nm_steps,
+               sps,
+               use_ramp=True,
+               return_to_start=True):
     GPIO.setmode(GPIO.BCM)
-    for pin in [MOTOR_Z["dir_pins"], MOTOR_Z["motor_pins"], MOTOR_X["dir_pins"], MOTOR_X["motor_pins"]]:
-        GPIO.setup(pin, GPIO.OUT)
+    pins = list(itertools.chain.from_iterable([motor[key] for key in motor.keys()]))
+    GPIO.setup(pins, GPIO.OUT)
+    print("initialized pins: {}".format(pins))
+
+    GPIO.output(motor["dir_pins"], direction)
+
+    if use_ramp:
+        delays = []
+
+        for x in range(1, nm_steps):
+            delay = (nm_steps - x) / sps
+            delays.append(delay)
+            GPIO.output(motor["motor_pins"], GPIO.HIGH)
+            sleep(delay)
+            GPIO.output(motor["motor_pins"], GPIO.LOW)
+            sleep(delay)
+        print("Used ramp values: {}".format(delays))
+
+    else:
+        for x in range(1, nm_steps):
+            delay = 1 / sps
+            GPIO.output(motor["motor_pins"], GPIO.HIGH)
+            sleep(delay)
+            GPIO.output(motor["motor_pins"], GPIO.LOW)
+            sleep(delay)
+
+    if return_to_start:
+        test_motor(motor,
+                   int(np.logical_not(direction)),
+                   nm_steps,
+                   sps,
+                   use_ramp,
+                   return_to_start=False)
+    GPIO.cleanup()
 
 
-def motor_test(DIR_pin, DIR, STEP_pin, steps, delay):
-    GPIO.output(DIR_pin, DIR)
-    for x in range(steps):
-        GPIO.output(STEP_pin, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(STEP_pin, GPIO.LOW)
-        sleep(delay)
-
-
-def ramp_motor(motor, direction, nm_steps, sps):
-    for direction_pin in motor["dir_pins"]:
-        GPIO.output(direction_pin, direction)
-    for x in range(1, nm_steps):
-        delay = 1 / (sps * (1 / (nm_steps - x)))
-
-        for motor_pin in motor["motor_pins"]:
-            GPIO.output(motor_pin, GPIO.HIGH)
-
-        sleep(delay)
-        for motor_pin in motor["motor_pins"]:
-            GPIO.output(motor_pin, GPIO.LOW)
-
-        sleep(delay)
-
-
-init_GPIO()
-
-# motor_test(config.DIR_1, config.CW, config.STEP_1, steps=50, delay=.005)
-# motor_test(config.DIR_1, config.CCW, config.STEP_1, steps=50, delay=.005)
-
-# motor_test(config.DIR_2, config.CW, config.STEP_2, steps=50, delay=.001)
-# motor_test(config.DIR_2, config.CCW, config.STEP_2, steps=50, delay=.002)
-
-ramp_motor(MOTOR_X, CW, 60, 80000)
-ramp_motor(MOTOR_X, CCW, 60, 80000)
-
-GPIO.cleanup()
+test_motor(MOTOR_X, CW, 60, 80000)
